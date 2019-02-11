daa795688f903c6dadf00f1a165d6f3481f22e82
Cardoso Filho <gerson.cardoso.filho@accenture.com>
2019-02-11 19:31:15 -0200

    Criacao de Dicionario

--- Model/Produto.cs
+++ Model/Produto.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Dynamic;
 using System.IO;
+using System.Linq;
 
 namespace ExcelApp.Model
 {
@@ -9,7 +11,7 @@ namespace ExcelApp.Model
     {
         private const string STRING_MASK = "{0};{1};{2};{3};{4};{5}";
 
-        public string CodigoOferta { get; set; }
+        public string CodigoOfertaVozComSVAComLDComVCFidel { get; set; }
         public string ConteudosOnLineFidel{ get; set; }
         public string AplicativosTitularFidel { get; set; }
         public string AplicativosDependenteFidel { get; set; }
@@ -22,7 +24,7 @@ namespace ExcelApp.Model
         {
             string[] values = csvLine.Split(';');
             Produto produtosBase = new Produto();
-            produtosBase.CodigoOferta = Convert.ToString(values[0]);
+            produtosBase.CodigoOfertaVozComSVAComLDComVCFidel = Convert.ToString(values[0]);
             produtosBase.ConteudosOnLineFidel = Convert.ToString(values[1]);
             produtosBase.AplicativosTitularFidel = Convert.ToString(values[2]);
             produtosBase.AplicativosDependenteFidel = Convert.ToString(values[3]);
@@ -32,48 +34,46 @@ namespace ExcelApp.Model
             return produtosBase;
         }
 
-        public static List<Produto> ComparaArquivos(List<Produto> produtosBase, List<Produto> produtosMapa)
+        public static List<Produto> ComparaArquivos(List<Produto> produtosBase, Dictionary<int, Produto> dictionaryProdutos)
         {
 
             Produto novoProduto;
             List<Produto> produtosNovos = new List<Produto>();
+            dynamic d1 = new ExpandoObject();
+
+            var dict = new Dictionary<string, dynamic>();
+
+            
 
             for(int i = 0; i < produtosBase.Count; i++)
             {
-                Debug.WriteLine("Codigo oferta base {0}", produtosBase[i].CodigoOferta);
-                Debug.WriteLine("Codigo oferta mapa {0}", produtosMapa[i].CodigoOferta);
-
                 novoProduto = new Produto();
 
-                if(produtosBase[i].CodigoOferta.Equals(produtosMapa[i].CodigoOferta))
+                var _oferta = produtosBase[i].CodigoOfertaVozComSVAComLDComVCFidel;
+
+                Debug.WriteLine("produtos base oferta:  " + _oferta);
+
+                novoProduto = Produto.SearchDictionary(produtosBase[i], dictionaryProdutos);
+
+                if(novoProduto != null)
                 {
-                    novoProduto.CodigoOferta = produtosMapa[i].CodigoOferta;
-                    novoProduto.ConteudosOnLineFidel = produtosMapa[i].ConteudosOnLineFidel;
-                    novoProduto.AplicativosTitularFidel = produtosMapa[i].AplicativosTitularFidel;
-                    novoProduto.AplicativosDependenteFidel = produtosMapa[i].AplicativosDependenteFidel;
-                    novoProduto.IsencoesTrafegoTitularFidel = produtosMapa[i].IsencoesTrafegoTitularFidel;
-                    novoProduto.IsencoesTrafegoDependenteFidel = produtosMapa[i].IsencoesTrafegoDependenteFidel;
                     produtosNovos.Add(novoProduto);
                 }
                 else
                 {
-                    novoProduto.CodigoOferta = produtosBase[i].CodigoOferta;
-                    novoProduto.ConteudosOnLineFidel = produtosBase[i].ConteudosOnLineFidel;
-                    novoProduto.AplicativosTitularFidel = produtosBase[i].AplicativosTitularFidel;
-                    novoProduto.AplicativosDependenteFidel = produtosBase[i].AplicativosDependenteFidel;
-                    novoProduto.IsencoesTrafegoTitularFidel = produtosBase[i].IsencoesTrafegoTitularFidel;
-                    novoProduto.IsencoesTrafegoDependenteFidel = produtosBase[i].IsencoesTrafegoDependenteFidel;
-                    produtosNovos.Add(novoProduto);
-
+                    produtosNovos.Add(produtosBase[i]);
                 }
 
             }
+
+           // var result = produtosBase.Where(r => produtosMapa.Any(r2 => r2.ConteudosOnLineFidel == r.ConteudosOnLineFidel)).ToList();
+
             return produtosNovos;
         }
 
         public static void SaveCSV (List<Produto> novosProdutos)
         {
-            using (var newFile = File.CreateText("//home//gerson//Documents//Repositories//NetCore//ExcelApp//testeNewProdutos.csv"))
+            using (var newFile = File.CreateText(@"C:\\repositories\\DotNet\\ExcelConsoleApp\\testeNewProdutos.csv"))
             {
                 foreach (var produto in novosProdutos)
                 {
@@ -84,12 +84,45 @@ namespace ExcelApp.Model
 
         public override string ToString()
         {
-           return string.Format(STRING_MASK, this.CodigoOferta, this.ConteudosOnLineFidel, this.AplicativosTitularFidel, this.AplicativosDependenteFidel, this.IsencoesTrafegoTitularFidel, this.IsencoesTrafegoDependenteFidel); 
+           return string.Format(STRING_MASK, this.CodigoOfertaVozComSVAComLDComVCFidel, this.ConteudosOnLineFidel, this.AplicativosTitularFidel, this.AplicativosDependenteFidel, this.IsencoesTrafegoTitularFidel, this.IsencoesTrafegoDependenteFidel); 
         }
 
+        public static Dictionary<int, Produto> CreateDictionary(List<Produto> listMapa)
+        {
+            Dictionary<int, Produto> dProduto = new Dictionary<int, Produto>();
 
+            for (int i = 0; i < listMapa.Count; i++)
+            {
+                dProduto.Add(i, listMapa[i]);
+            }
+
+            return dProduto;
+        }
+
+        public static void ShowDictionary (Dictionary<int, Produto> dictionaryProdutos)
+        {
+            foreach (var keyValuePair in dictionaryProdutos.Values)
+            {
+                Debug.WriteLine(keyValuePair.CodigoOfertaVozComSVAComLDComVCFidel);
+            }
+        }
+
+        public static Produto SearchDictionary(Produto produto, Dictionary<int, Produto> dictionaryProdutos)
+        {
+            var oldProduto = produto;
+            Produto newProduto;
+
+            foreach (var keyValuePair in dictionaryProdutos)
+            {
+                newProduto = new Produto();
+
+                if (keyValuePair.Value.CodigoOfertaVozComSVAComLDComVCFidel.Contains(oldProduto.CodigoOfertaVozComSVAComLDComVCFidel))      
+                {
+                    newProduto = keyValuePair.Value;
+                }
+                return newProduto;
+            }
+            return null;
+        }
     }
-
-    
-
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using ExcelApp.Model;
@@ -12,23 +13,34 @@ namespace ExcelApp
         static void Main(string[] args)
         {
             List<Produto> produtosBase = File
-                                        .ReadAllLines("//home//gerson//Documents//Repositories//NetCore//ExcelApp//teste.csv")
+                                        .ReadAllLines(@"C:\\repositories\\DotNet\\ExcelConsoleApp//dtBase.csv")
                                         .Skip(1)
                                         .Select( v => Produto.FromCsv(v))
                                         .ToList();
+
+            Debug.WriteLine("Quantidade de linhas do arquivo base: ", produtosBase.Count.ToString());
+                                        
             
 
             List<Produto> produtosMapa = File
-                                        .ReadAllLines("//home//gerson//Documents//Repositories//NetCore//ExcelApp//testeMapa.csv")
+                                        .ReadAllLines(@"C:\\repositories\\DotNet\\ExcelConsoleApp//dtMapa.csv")
                                         .Skip(1)
                                         .Select( v => Produto.FromCsv(v))
                                         .ToList();
 
-            var novosProdutos = Produto.ComparaArquivos(produtosBase,produtosMapa);
 
-            Produto.SaveCSV(novosProdutos);
 
-            Console.WriteLine("Arquivo gravado com sucesso!");
+
+            // Debug.WriteLine("Quantidade de linhas do arquivo mapa: ", produtosMapa.Count.ToString());
+
+            // var novosProdutos = Produto.ComparaArquivos(produtosBase,produtosMapa);
+
+            // Produto.SaveCSV(novosProdutos);
+
+            // Console.WriteLine("Arquivo gravado com sucesso!");
+
+            var produtosDictionary = Produto.CreateDictionary(produtosMapa);
+            var novosProdutos = Produto.ComparaArquivos(produtosBase,produtosDictionary);            
         }
     }
 }
