13d781955535d57e5dd3a9fb628e36550e2fde64
Cardoso Filho <gerson.cardoso.filho@accenture.com>
2019-02-11 20:50:43 -0200

    Inserção de comentários

--- Program.cs
+++ Program.cs
@@ -32,12 +32,20 @@ namespace ExcelApp
 
 
             // Debug.WriteLine("Quantidade de linhas do arquivo mapa: ", produtosMapa.Count.ToString());
-
             // var novosProdutos = Produto.ComparaArquivos(produtosBase,produtosMapa);
-
             // Produto.SaveCSV(novosProdutos);
-
             // Console.WriteLine("Arquivo gravado com sucesso!");
+
+            //Passos:
+            // Carrega as duas tabelas em List<Produto>
+            // o arquivo dtBase é o arquivo a ser atualizado
+            // o arquivo dtMapa é o arquivo completo enviado pelo cliente
+
+            // depois de preenchidas as duas listas, crio um Dicionário da dtMapa
+            // depois rodo o método que compara os dois arquivos, a List<Produto> e o Dictionary<int, Produto>
+            // a variável novosProdutos deveria conter a lista dtBase atualizada. Mas pelo que vi está fazendo apenas
+            // a primeira linha
+
 
             var produtosDictionary = Produto.CreateDictionary(produtosMapa);
             var novosProdutos = Produto.ComparaArquivos(produtosBase,produtosDictionary);            
